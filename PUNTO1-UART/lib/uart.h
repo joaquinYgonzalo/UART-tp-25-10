a9f791cf80a93fc9940176743401cc8b7725f659
joaquinYgonzalo <166860732+joaquinYgonzalo@users.noreply.github.com>
2024-11-01 16:17:58 -0300

    Delete PUNTO1-UART/lib/uart.h

--- PUNTO1-UART/lib/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef "uart_h"
-#define "uart_h"
-char UART0_SEND(char dato);
-void UART_SendString(const char* str);
-void UART0_INIT(uint32_t baud_rate);
-char HC05_RECIBE(void);
-void UART_ReceiveString(char* palabra, uint16_t tMax);
-
-#endif
